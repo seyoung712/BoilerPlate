7d01f9704f10a26e3dc063872c72fff5c65cb88f
seyoung712 <hsj0069@naver.com>
2023-02-03 23:37:49 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Seyoung:1234qaz@cluster0.ueso7yi.mongodb.net/?retryWrites=true&w=majority')
+const config = require("./config/key");
+
+
+const {User} = require('./models/User');
+
+//application/x-www-form-urlencoded를 분석해서 가져옴
+app.use(bodyParser.urlencoded({extended: true}));
+
+//aplication/json으로 된 것을 분석해 가져옴
+app.use(bodyParser.json());
+
+
+//MongoDB 연결 (config/dev.js)
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected'))
 .catch((e) => console.log('MongoDB error: ', e));
 
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!, 새해 복 많이 받으세요.')
 })
+
+//회원가입을 위한 Router
+app.post('/register', (req, res) => {
+
+    //회원가입에 필요한 정보들을 Client에서 가져오면 DB에 저장
+    const user = new User(req.body)
+
+    //user에 저장
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+
+
+const User = mongoose.model('User', userSchema) //userSchema를 model로 감싸줌
+
+module.exports= { User } //다른 파일에서도 사용할 수 있도록 설정
